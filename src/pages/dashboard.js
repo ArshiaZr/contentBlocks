3b3c679a0dd07016d06854c947a2da84ea0158c8
Arshia Zakeri Rad <zrr.arshia@gmail.com>
2024-05-30 11:53:20 -0400

    Added logout logic on Dashboard page

--- src/pages/dashboard.js
+++ src/pages/dashboard.js
@@ -1,8 +1,30 @@
+import React from "react";
+import { useClerk } from "@clerk/clerk-react";
+
 export default function Dashboard() {
+  const { signOut } = useClerk();
+
+  const handleLogout = async () => {
+    await signOut();
+  };
+
   return (
-    <div>
-      <h1>Dashboard</h1>
-      <p>Welcome to the dashboard</p>
-    </div>
+    <main className="background">
+      <div className="max-w-lg mx-auto p-8 text-center ">
+        <div className="title-class">Dashboard</div>
+        <div className="text-white text-lg mt-4">
+          This is a protected route. You can only see this if you are logged in.
+        </div>
+        <div className="mt-8">
+          <button
+            type="button"
+            onClick={handleLogout}
+            className="text-white text-lg bg-purple-500 p-2 rounded-md hover:bg-purple-400 pointer"
+          >
+            Logout
+          </button>
+        </div>
+      </div>
+    </main>
   );
 }
